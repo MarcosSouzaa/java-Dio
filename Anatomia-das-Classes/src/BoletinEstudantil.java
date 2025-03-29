2750a645f1f36309d29f792d4bdf7525c9b1bd0d
MarcosSouzaa <marcossouza.git@gmail.com>
2025-03-29 16:09:57 -0300

    Identação

---
+++ Anatomia-das-Classes/src/BoletinEstudantil.java
@@ -0,0 +1,16 @@
+public class BoletinEstudantil {
+    
+    // IDENTAÇÃO
+    public static void main(String[] args) {
+
+        int mediaFinal = 8;
+
+        if (mediaFinal < 7)
+            System.out.println("REPROVADO!");
+        else if (mediaFinal == 7)
+            System.out.println("PROVA MINERVA");
+        else 
+            System.out.println("APROVAADO");     
+               
+    }
+}
